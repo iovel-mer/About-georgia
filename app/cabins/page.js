beda2a709f7502561cab78119e44dd8c11735941
iovel-mer <ioveeel.mind@gmail.com>
2024-12-20 18:54:10 +0400

    Added Spinner,Id and error

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,36 +1,29 @@
-import CabinCard from "@/app/_components/CabinCard";
-import { getCabins } from "../_lib/data-service";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
-    title: "Cabin"
-  }
+  title: "Cabin",
+};
 
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+        Our Luxury Cabins
+      </h1>
+      <p className="text-primary-200 text-lg mb-10">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
 
-  export default async function Page() {
-   
-    const cabins = await getCabins()
-  
-    return (
-      <div>
-        <h1 className="text-4xl mb-5 text-accent-400 font-medium">
-          Our Luxury Cabins
-        </h1>
-        <p className="text-primary-200 text-lg mb-10">
-          Cozy yet luxurious cabins, located right in the heart of the Italian
-          Dolomites. Imagine waking up to beautiful mountain views, spending your
-          days exploring the dark forests around, or just relaxing in your private
-          hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
-          away from home. The perfect spot for a peaceful, calm vacation. Welcome
-          to paradise.
-        </p>
-  
-        {cabins.length > 0 && (
-          <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-            {cabins.map((cabin) => (
-              <CabinCard cabin={cabin} key={cabin.id} />
-            ))}
-          </div>
-        )}
-      </div>
-    );
-  } 
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
+    </div>
+  );
+}
--- app/loading.js
+++ app/loading.js
@@ -1,5 +1,10 @@
+import Spinner from "./_components/Spinner";
+
 export default function Loading() {
 return (
-    <h1>Loading</h1>
+    <div className="grid items-center justify-center">
+        <Spinner/>
+        <p className="text-xl text-primary-200">Loading cabin data..</p>
+    </div>
 )
 }
